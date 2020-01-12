a246ad797474c278372940323898c12eec5138cd
uglefugl <59791868+uglefugl@users.noreply.github.com>
2020-01-12 12:01:02 +0100

    nu med liv

--- main.ts
+++ main.ts
@@ -1,3 +1,6 @@
+namespace SpriteKind {
+    export const Block = SpriteKind.create()
+}
 namespace myTiles {
     //% blockIdentity=images._tile
     export const tile0 = img`
@@ -58,6 +61,10 @@ f f f f f f f f f f f f f f f f
 `
     //% blockIdentity=images._tile
     export const tile3 = img`
+3 3 3 3 . . . . . . . . . . . . 
+3 3 3 3 . . . . . . . . . . . . 
+3 3 3 3 . . . . . . . . . . . . 
+3 3 3 3 . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
@@ -67,10 +74,6 @@ f f f f f f f f f f f f f f f f
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
-. 3 3 3 3 . . . . . . . . . . . 
-. 3 3 3 3 . . . . . . . . . . . 
-. 3 3 3 3 . . . . . . . . . . . 
-. 3 3 3 3 . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
@@ -95,21 +98,39 @@ f f f f f f f f f f f f f f f f
 . . . . . . . . . . . . . . . . 
 `
 }
-info.onCountdownEnd(function () {
+controller.B.onEvent(ControllerButtonEvent.Pressed, function () {
+    pladehastighed += 5
+})
+sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Block, function (sprite, otherSprite) {
+    info.changeLifeBy(-1)
+    otherSprite.destroy(effects.disintegrate, 200)
+    if (Math.abs(sprite.x - otherSprite.x) > Math.abs(sprite.y - otherSprite.y)) {
+        sprite.vx = 0 - sprite.vx
+    } else {
+        sprite.vy = 0 - sprite.vy
+    }
+})
+controller.A.onEvent(ControllerButtonEvent.Pressed, function () {
+    pladehastighed += -5
+})
+info.onLifeZero(function () {
     game.over(false, effects.bubbles)
 })
 sprites.onOverlap(SpriteKind.Projectile, SpriteKind.Player, function (sprite, otherSprite) {
     info.changeScoreBy(1)
-    mySprite.setVelocity(mySprite.vx, 0 - mySprite.vy)
+    mySprite.vy = 0 - mySprite.vy
 })
+let mySprite2: Sprite = null
 let mySprite: Sprite = null
+let pladehastighed = 0
+let plade: Sprite = null
 tiles.setTilemap(tiles.createTilemap(
-            hex`1000100003030303030303030303030303030303030303030301030303030303030303030303030303030303030303030303030303030301030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303`,
+            hex`1000100003030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303030303`,
             img`
 . . . . . . . . . . . . . . . . 
-. . . . . 2 . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
-. . . 2 . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
+. . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
 . . . . . . . . . . . . . . . . 
@@ -126,8 +147,10 @@ tiles.setTilemap(tiles.createTilemap(
             [myTiles.tile0,myTiles.tile1,myTiles.tile2,myTiles.tile3,myTiles.tile4],
             TileScale.Sixteen
         ))
-info.startCountdown(60)
 info.setScore(0)
+info.setLife(240)
+controller.moveSprite(plade, pladehastighed, 0)
+pladehastighed = 100
 mySprite = sprites.create(img`
 . 3 3 3 . 
 3 3 3 3 3 
@@ -136,15 +159,33 @@ mySprite = sprites.create(img`
 . 3 3 3 . 
 `, SpriteKind.Projectile)
 mySprite.setVelocity(100, 100)
-let plade = sprites.create(img`
+plade = sprites.create(img`
 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 3 
 `, SpriteKind.Player)
-plade.y = 100
-controller.moveSprite(plade, 100, 0)
+plade.y = 103
 mySprite.setFlag(SpriteFlag.StayInScreen, true)
 plade.setFlag(SpriteFlag.StayInScreen, true)
 mySprite.setFlag(SpriteFlag.BounceOnWall, true)
+mySprite.setPosition(75, 107)
+for (let indexy = 0; indexy <= 11; indexy++) {
+    for (let index = 0; index <= 19; index++) {
+        mySprite2 = sprites.create(img`
+8 8 8 8 8 8 8 8 
+8 8 8 8 8 8 8 8 
+8 8 8 8 8 8 8 8 
+8 8 8 8 8 8 8 8 
+8 8 8 8 8 8 8 8 
+8 8 8 8 8 8 8 8 
+8 8 8 8 8 8 8 8 
+8 8 8 8 8 8 8 8 
+`, SpriteKind.Block)
+        mySprite2.setPosition(4 + 8 * index, 4 + 8 * indexy)
+    }
+}
+game.onUpdateInterval(500, function () {
+    controller.moveSprite(plade, pladehastighed, 0)
+})
